116e5ebd39db0acec75044c02201a48df6d360ed
Valentin Terebenin <terebenin.work@gmail.com>
2023-12-31 17:29:00 +0300

    first commit

---
+++ src/main.rs
@@ -0,0 +1,33 @@
+use iced::{Sandbox, widget::text, Settings};
+
+fn main() -> iced::Result {
+    Editor::run(Settings::default())
+}
+
+struct Editor;
+
+#[derive(Debug)]
+enum Message {
+    
+}
+
+impl Sandbox for Editor {
+    type Message = Message;
+
+    fn new() -> Self {
+        Self
+    }
+
+    fn title(&self) -> String {
+        String::from("A cool editor?")
+    }
+
+    fn update(&mut self, message: Message) {
+        match message {}
+    }
+
+    fn view(&self) -> iced::Element<'_, Message> {
+        text("Hello, iced!").into()
+    }
+}
+
