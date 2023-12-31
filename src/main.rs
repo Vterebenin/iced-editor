2594868f646f69560b29ffc3d3cd7c56a199dc6e
Valentin Terebenin <terebenin.work@gmail.com>
2023-12-31 18:31:27 +0300

    feat: added async runtime loading

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,11 @@
-use iced::{Sandbox, widget::{column, row, text_editor, container, TextEditor, text, horizontal_space, Row}, Settings, Length};
+use iced::{
+    executor,
+    widget::{column, container, horizontal_space, row, text, text_editor, TextEditor},
+    Application, Command, Length, Sandbox, Settings, Theme,
+};
+use std::io;
+use std::path::Path;
+use std::sync::Arc;
 
 fn main() -> iced::Result {
     Editor::run(Settings::default())
@@ -10,34 +17,50 @@ struct Editor {
 
 #[derive(Debug, Clone)]
 enum Message {
-    Edit(text_editor::Action)
+    Edit(text_editor::Action),
+    FileOpened(Result<Arc<String>, io::ErrorKind>),
 }
 
-impl Sandbox for Editor {
+impl Application for Editor {
     type Message = Message;
+    type Theme = Theme;
+    type Executor = executor::Default;
+    type Flags = ();
 
-    fn new() -> Self {
-        Self {
-            content: text_editor::Content::with(include_str!("main.rs")),
-        }
+    fn new(_flags: Self::Flags) -> (Self, Command<Message>) {
+        (
+            Self {
+                content: text_editor::Content::new()
+            },
+            Command::perform(load_file(format!(
+                "{}/src/main.rs",
+                env!("CARGO_MANIFEST_DIR")
+            )), Message::FileOpened),
+        )
     }
 
     fn title(&self) -> String {
         String::from("A cool editor")
     }
 
-    fn update(&mut self, message: Message) {
+    fn update(&mut self, message: Message) -> Command<Message> {
         match message {
             Message::Edit(action) => {
                 self.content.edit(action);
+            },
+            Message::FileOpened(result) => {
+                if let Ok(content) = result {
+                    self.content = text_editor::Content::with(&content);
+                }
             }
+
         }
+
+        Command::none()
     }
 
     fn view(&self) -> iced::Element<'_, Message> {
-        let input: TextEditor<_, _> = text_editor(&self.content)
-            .on_edit(Message::Edit)
-            .into();
+        let input: TextEditor<_, _> = text_editor(&self.content).on_edit(Message::Edit).into();
 
         let position = {
             let (line, column) = self.content.cursor_position();
@@ -55,3 +78,9 @@ impl Sandbox for Editor {
     }
 }
 
+async fn load_file(path: impl AsRef<Path>) -> Result<Arc<String>, io::ErrorKind> {
+    tokio::fs::read_to_string(path)
+        .await
+        .map(Arc::new)
+        .map_err(|error| error.kind())
+}
