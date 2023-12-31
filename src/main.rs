95209ec90c7491aafe997453bdbc2168ced42765
Valentin Terebenin <terebenin.work@gmail.com>
2023-12-31 17:38:51 +0300

    feat: multiline

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,25 @@
-use iced::{Sandbox, widget::text, Settings};
+use iced::{Sandbox, widget::{text_editor, container, TextEditor}, Settings};
 
 fn main() -> iced::Result {
     Editor::run(Settings::default())
 }
 
-struct Editor;
+struct Editor {
+    content: text_editor::Content,
+}
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 enum Message {
-    
+    Edit(text_editor::Action)
 }
 
 impl Sandbox for Editor {
     type Message = Message;
 
     fn new() -> Self {
-        Self
+        Self {
+            content: text_editor::Content::new(),
+        }
     }
 
     fn title(&self) -> String {
@@ -23,11 +27,19 @@ impl Sandbox for Editor {
     }
 
     fn update(&mut self, message: Message) {
-        match message {}
+        match message {
+            Message::Edit(action) => {
+                self.content.edit(action);
+            }
+        }
     }
 
     fn view(&self) -> iced::Element<'_, Message> {
-        text("Hello, iced!").into()
+        let input: TextEditor<_, _> = text_editor(&self.content)
+            .on_edit(Message::Edit)
+            .into();
+
+        container(input).padding(10).into()
     }
 }
 
